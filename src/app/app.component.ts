fdddfad7302cbd156ce8d19c8630729be4413711
Aliaksandr Siarohin <a.siarohin@gmail.com>
2022-11-25 10:45:12 +0600

    refactor: optimize Change Detection (#9)
    Co-authored-by: Aliaksandr Siarohin <aliaksandr_siarohin@epam.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SpinnerService } from './core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   constructor(public spinnerService: SpinnerService) {}
--- src/app/modules/favorites/favorites.component.ts
+++ src/app/modules/favorites/favorites.component.ts
@@ -1,22 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { FavoritesStoreFacade, IPhoto } from '../../core';
-import { Observable, share } from 'rxjs';
 import { Path } from '../../constants';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
-  styleUrls: ['./favorites.component.scss']
+  styleUrls: ['./favorites.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FavoritesComponent implements OnInit {
-  constructor(private favoritesStoreFacade: FavoritesStoreFacade, private router: Router) {}
+export class FavoritesComponent implements OnInit, OnDestroy {
+  private subscription = new Subscription();
 
-  public photos$: Observable<IPhoto[]>;
+  public photos: IPhoto[];
+
+  constructor(private favoritesStoreFacade: FavoritesStoreFacade, private router: Router) {}
 
   public ngOnInit(): void {
     this.favoritesStoreFacade.loadFavorites();
-    this.photos$ = this.favoritesStoreFacade.photos$.pipe(share());
+    this.subscription = this.favoritesStoreFacade.photos$.subscribe((photos: IPhoto[]) => this.photos = photos);
+  }
+
+  public ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
   public navigate(photo: IPhoto): void {
